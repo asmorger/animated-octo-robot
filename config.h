63027719249e804261df10a98351448c93ca1295
Andrew S. Morger <asmorger@live.com>
2022-10-15 18:45:27 -0500

    Defines tapping and permissive hold

--- config.h
+++ config.h
@@ -28,8 +28,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-//#define TAPPING_FORCE_HOLD
-//#define TAPPING_TERM 100
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 150
+#define PERMISSIVE_HOLD
+
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
