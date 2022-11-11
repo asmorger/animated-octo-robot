4593df4f27dcb026bf38ed545ab0a5868e02a78c
Andrew Morger <asmorger@live.com>
2022-11-10 21:26:48 -0600

    Configures build/flashing from cli

--- ferris/build.sh
+++ ferris/build.sh
@@ -1,2 +1,2 @@
 qmk compile -kb ferris/sweep -km hawkbane
-cp ~/qmk_firmware/.build/ferris_sweep_hawkbane.hex /mnt/c/temp/sweep.hex
+# :cp ~/qmk_firmware/.build/ferris_sweep_hawkbane.hex /mnt/c/temp/sweep.hex
