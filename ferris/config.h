eed5412c175d1bf858738eb9c86f4bb7bedd44a7
Andrew Morger <asmorger@live.com>
2022-11-10 21:03:12 -0600

    Updates tapping rules to match Ben Vallack

--- ferris/config.h
+++ ferris/config.h
@@ -18,14 +18,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 #define MASTER_LEFT
 
-#define LEADER_PER_KEY_TIMING
-#define LEADER_TIMEOUT 250
-
-#define TAPPING_TERM 150
-#define TAPPING_TERM_PER_KEY
-#define LONG_TAPPING_TERM 1000
-#define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
+#undef TAPPING_TERM
+#define TAPPING_TERM 240
+#define PERMISSIVE_HOLD
 
 #define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
 #define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
