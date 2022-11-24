1dd938a05580896b72bd3ed5e9e9405811dfca29
Andrew Morger <asmorger@live.com>
2022-11-24 15:46:20 -0600

    Adds callum keymap

---
+++ ferris-callum/flash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Flashing Hawkbane's Callum keymap in 2 seconds..."
+
+sleep 2s
+
+qmk flash -kb ferris/sweep -km hawkbane-callum
---
+++ ferris-callum/link.sh
@@ -0,0 +1,1 @@
+ln -s ~/dev/animated-octo-robot/ferris-callum ~/qmk_firmware/keyboards/ferris/keymaps/hawkbane-callum
