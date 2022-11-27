dde8fe7a333a78784af75ac9339d4d3154cc5821
Andrew Morger <asmorger@live.com>
2022-11-26 21:29:32 -0600

    Moves tilde and braces around

--- ferris-callum/keymap.c
+++ ferris-callum/keymap.c
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [SYM] = LAYOUT_split_3x5_2(
-        KC_ESC,  KC_LBRC, KC_LCBR, KC_LPRN, KC_AT,   KC_CIRC, KC_RPRN, KC_RCBR, KC_RBRC, KC_GRV,
+        KC_ESC,  KC_LPRN, KC_RPRN, KC_LT,   KC_GT,   KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_GRV,
         KC_MINS, KC_ASTR, KC_EQL,  KC_UNDS, KC_DLR,  KC_HASH, OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT,
-        KC_PLUS, KC_PIPE, KC_TILD, KC_SLSH, KC_PERC, XXXXXXX, KC_BSLS, KC_AMPR, KC_QUES, KC_EXLM,
+        KC_PLUS, KC_PIPE, KC_TILD, KC_SLSH, KC_PERC, KC_AT,   KC_BSLS, KC_AMPR, KC_QUES, KC_EXLM,
                                    _______, LCTL(KC_BSPC), _______, _______
     ),
 
