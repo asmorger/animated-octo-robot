206dc2d6962e757c5824e31f8d6b0d0310655e7d
Andrew Morger <asmorger@live.com>
2022-11-30 13:55:31 -0600

    Adds quotes

--- ferris-callum/keymap.c
+++ ferris-callum/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DEF] = LAYOUT_split_3x5_2(
         KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
         KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
-        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  CAPSWRD,
+        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_QUOT,
                                    LA_NAV,  KC_SPC,  UI,      LA_SYM
     ),
 
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  SW_WIN,  TAB_L,   TAB_R,    RESET,  KC_HOME, KC_PGDN, KC_PGUP,KC_END,  KC_DEL,
         OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, KC_BSPC,
         SPACE_L, SPACE_R, XXXXXXX,XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,_______, KC_ENT,
-                                   _______, _______, _______, _______
+                                   _______, _______, CAPSWRD, _______
     ),
 
     [NUM] = LAYOUT_split_3x5_2(
