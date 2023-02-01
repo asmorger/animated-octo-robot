de813175427a16bae6ccebf8ed5fac3fceaedd9f
Andrew Morger <asmorger@users.noreply.github.com>
2023-01-31 19:36:50 -0600

    Adds repeat functionality.

--- ferris-callum/keymap.c
+++ ferris-callum/keymap.c
@@ -33,6 +33,7 @@ enum keycodes {
 
     SW_WIN,  // Switch to next window         (cmd-tab)
     SW_LANG, // Switch to next input language (ctl-spc)
+    REPEAT, // https://gist.github.com/NotGate/3e3d8ab81300a86522b2c2549f99b131?permalink_comment_id=4282534#gistcomment-4282534
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -47,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_LPRN, KC_RPRN, KC_LT,   KC_GT,   KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_GRV,
         KC_MINS, KC_ASTR, KC_EQL,  KC_UNDS, KC_DLR,  KC_HASH, OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT,
         KC_PLUS, KC_PIPE, KC_TILD, KC_SLSH, KC_PERC, KC_AT,   KC_BSLS, KC_AMPR, KC_QUES, KC_EXLM,
-                                   _______, LCTL(KC_BSPC), _______, _______
+                                   _______, REPEAT,  _______, _______
     ),
 
     [NAV] = LAYOUT_split_3x5_2(
@@ -59,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [NUM] = LAYOUT_split_3x5_2(
         KC_7,    KC_5,    KC_3,    KC_1,    KC_9,    KC_8,    KC_0,    KC_2,    KC_4,    KC_6,
-        OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  KC_F11,  KC_F10,  OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT,
+        OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  KC_F11,  KC_F10,  OS_CMD,  OS_ALT,  OS_CTRL, LCTL(KC_BSPC),
         XXXXXXX, XXXXXXX, TEST,    BUILD,   XXXXXXX, BACK,    KC_VOLD, KC_VOLU, FWD,     XXXXXXX,
                                    _______, _______, _______, _______
     ),
@@ -97,7 +98,55 @@ oneshot_state os_ctrl_state = os_up_unqueued;
 oneshot_state os_alt_state = os_up_unqueued;
 oneshot_state os_cmd_state = os_up_unqueued;
 
+// Used to extract the basic tapping keycode from a dual-role key.
+// Example: GET_TAP_KC(MT(MOD_RSFT, KC_E)) == KC_E
+#define GET_TAP_KC(dual_role_key) dual_role_key & 0xFF
+uint16_t last_keycode = KC_NO;
+uint8_t last_modifier = 0;
+uint16_t pressed_keycode = KC_NO;
+
+void process_repeat_key(uint16_t keycode, const keyrecord_t *record) {
+  if (keycode != REPEAT) {
+    // Early return when holding down a pure layer key
+    // to retain modifiers
+    switch (keycode) {
+      case QK_DEF_LAYER ... QK_DEF_LAYER_MAX:
+      case QK_MOMENTARY ... QK_MOMENTARY_MAX:
+      case QK_LAYER_MOD ... QK_LAYER_MOD_MAX:
+      case QK_ONE_SHOT_LAYER ... QK_ONE_SHOT_LAYER_MAX:
+      case QK_TOGGLE_LAYER ... QK_TOGGLE_LAYER_MAX:
+      case QK_TO ... QK_TO_MAX:
+      case QK_LAYER_TAP_TOGGLE ... QK_LAYER_TAP_TOGGLE_MAX:
+      case QK_MODS ... QK_MODS_MAX:
+        return;
+    }
+    if (record->event.pressed) {
+      last_modifier = get_mods() | get_oneshot_mods();
+      switch (keycode) {
+        case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
+        case QK_MOD_TAP ... QK_MOD_TAP_MAX:
+          last_keycode = GET_TAP_KC(keycode);
+          break;
+        default:
+          last_keycode = keycode;
+          break;
+        }
+    }
+  } else { // keycode == REPEAT
+    if (record->event.pressed) {
+      pressed_keycode = last_keycode;
+      register_mods(last_modifier);
+      register_code16(pressed_keycode);
+      unregister_mods(last_modifier);
+    } else {
+      unregister_code16(pressed_keycode);
+    }
+  }
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    process_repeat_key(keycode, record);
+    
     update_swapper(
         &sw_win_active, KC_LGUI, KC_TAB, SW_WIN,
         keycode, record
