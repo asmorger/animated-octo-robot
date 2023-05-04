cb0f669785cc69b7f69d2f6f8b1957277284f92f
Andrew Morger <asmorger@live.com>
2023-05-04 06:20:22 -0500

    Enables trackball and moves master to the right

--- ferris-callum/keymap.c
+++ ferris-callum/keymap.c
@@ -2,6 +2,7 @@
 
 #include "oneshot.h"
 #include "swapper.h"
+#include "drivers/sensors/pimoroni_trackball.h"
 
 #define HOME G(KC_LEFT)
 #define END G(KC_RGHT)
@@ -53,13 +54,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_LPRN, KC_LT,   KC_LBRC, KC_LCBR, KC_RCBR, KC_RBRC,   KC_GT, KC_RPRN,  KC_GRV,
         KC_MINS, KC_ASTR, KC_EQL,  KC_UNDS, KC_DLR,  KC_AT,    OS_CMD,  OS_ALT, OS_CTRL, OS_SHFT,
         KC_PLUS, KC_PIPE, KC_TILD, KC_SLSH, KC_PERC, KC_HASH, KC_BSLS, KC_AMPR, KC_QUES, KC_EXLM,
-                                   _______, REPEAT,  _______, _______
+                                   _______, _______,  _______, _______
     ),
 
     [NAV] = LAYOUT_split_3x5_2(
-        KC_TAB,  SW_WIN,  TAB_L,   TAB_R,  QK_BOOT,  KC_HOME, KC_PGDN, KC_PGUP,KC_END,  KC_DEL,
+        KC_TAB,  SW_WIN,  TAB_L,   TAB_R,   XXXXXXX,  KC_HOME, KC_PGDN, KC_PGUP,KC_END,  KC_DEL,
         OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, KC_BSPC,
-        SPACE_L, SPACE_R, XXXXXXX,XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,_______, KC_ENT,
+        SPACE_L, SPACE_R, XXXXXXX,XXXXXXX,  QK_BOOT, QK_BOOT, XXXXXXX, XXXXXXX,_______, KC_ENT,
                                    _______, _______, _______, _______
     ),
 
@@ -71,116 +72,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
-bool is_oneshot_cancel_key(uint16_t keycode) {
-    switch (keycode) {
-    case LA_SYM:
-    case LA_NAV:
-        return true;
-    default:
-        return false;
-    }
-}
-bool is_oneshot_ignored_key(uint16_t keycode) {
-    switch (keycode) {
-    case LA_SYM:
-    case LA_NAV:
-    case KC_LSFT:
-    case OS_SHFT:
-    case OS_CTRL:
-    case OS_ALT:
-    case OS_CMD:
-        return true;
-    default:
-        return false;
-    }
-}
-
-bool sw_win_active = false;
-bool sw_lang_active = false;
-
-oneshot_state os_shft_state = os_up_unqueued;
-oneshot_state os_ctrl_state = os_up_unqueued;
-oneshot_state os_alt_state = os_up_unqueued;
-oneshot_state os_cmd_state = os_up_unqueued;
-
-// Used to extract the basic tapping keycode from a dual-role key.
-// Example: GET_TAP_KC(MT(MOD_RSFT, KC_E)) == KC_E
-#define GET_TAP_KC(dual_role_key) dual_role_key & 0xFF
-uint16_t last_keycode = KC_NO;
-uint8_t last_modifier = 0;
-uint16_t pressed_keycode = KC_NO;
-
-void process_repeat_key(uint16_t keycode, const keyrecord_t *record) {
-  if (keycode != REPEAT) {
-    // Early return when holding down a pure layer key
-    // to retain modifiers
-    switch (keycode) {
-      case QK_DEF_LAYER ... QK_DEF_LAYER_MAX:
-      case QK_MOMENTARY ... QK_MOMENTARY_MAX:
-      case QK_LAYER_MOD ... QK_LAYER_MOD_MAX:
-      case QK_ONE_SHOT_LAYER ... QK_ONE_SHOT_LAYER_MAX:
-      case QK_TOGGLE_LAYER ... QK_TOGGLE_LAYER_MAX:
-      case QK_TO ... QK_TO_MAX:
-      case QK_LAYER_TAP_TOGGLE ... QK_LAYER_TAP_TOGGLE_MAX:
-      case QK_MODS ... QK_MODS_MAX:
-        return;
-    }
-    if (record->event.pressed) {
-      last_modifier = get_mods() | get_oneshot_mods();
-      switch (keycode) {
-        case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
-        case QK_MOD_TAP ... QK_MOD_TAP_MAX:
-          last_keycode = GET_TAP_KC(keycode);
-          break;
-        default:
-          last_keycode = keycode;
-          break;
-        }
-    }
-  } else { // keycode == REPEAT
-    if (record->event.pressed) {
-      pressed_keycode = last_keycode;
-      register_mods(last_modifier);
-      register_code16(pressed_keycode);
-      unregister_mods(last_modifier);
-    } else {
-      unregister_code16(pressed_keycode);
-    }
-  }
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    process_repeat_key(keycode, record);
-    
-    update_swapper(
-        &sw_win_active, KC_LGUI, KC_TAB, SW_WIN,
-        keycode, record
-    );
-    update_swapper(
-        &sw_lang_active, KC_LCTL, KC_SPC, SW_LANG,
-        keycode, record
-    );
-
-    update_oneshot(
-        &os_shft_state, KC_LSFT, OS_SHFT,
-        keycode, record
-    );
-    update_oneshot(
-        &os_ctrl_state, KC_LCTL, OS_CTRL,
-        keycode, record
-    );
-    update_oneshot(
-        &os_alt_state, KC_LALT, OS_ALT,
-        keycode, record
-    );
-    update_oneshot(
-        &os_cmd_state, KC_LCMD, OS_CMD,
-        keycode, record
-    );
-
-    return true;
-}
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, SYM, NAV, NUM);
-}
--- ferris-callum/rules.mk
+++ ferris-callum/rules.mk
@@ -2,8 +2,14 @@ RGBLIGHT_ENABLE = no
 CAPS_WORD_ENABLE = yes
 AUTO_SHIFT_ENABLE = yes
 
+POINTING_DEVICE_ENABLE = yes	# Enable Pimoroni Trackball on master (+2304)
+POINTING_DEVICE_DRIVER = pimoroni_trackball
+
+QUANTUM_LIB_SRC += i2c_master.c
+
 SRC += oneshot.c
 SRC += swapper.c
+SRC += drivers/sensors/pimoroni_trackball.c
 
 # Bootloader selection
 BOOTLOADER = atmel-dfu
