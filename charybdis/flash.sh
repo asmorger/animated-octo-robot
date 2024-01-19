d6715d4880458f861efcbbda25431ecef2737c1b
Andrew Morger <asmorger@live.com>
2024-01-19 14:58:17 -0600

    Adds keymap for Charybdis Nano

---
+++ charybdis/flash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Flashing Hawkbane's Charybdis keymap in 2 seconds..."
+
+sleep 2s
+
+qmk flash -c -kb bastardkb/charybdis/3x5/v2/splinky_3 -km hawkbane
---
+++ charybdis/link.sh
@@ -0,0 +1,1 @@
+ln -s $PWD  ~/qmk_firmware/keyboards/bastardkb/charybdis/3x5/keymaps/hawkbane
