53fe8b11b98994e6b2a9975942b5383805414f91
Andrew Morger <asmorger@live.com>
2024-01-20 06:54:15 -0600

    Removes compilation flag from flash script

--- charybdis/flash.sh
+++ charybdis/flash.sh
@@ -4,4 +4,4 @@ echo "Flashing Hawkbane's Charybdis keymap in 2 seconds..."
 
 sleep 2s
 
-qmk flash -c -kb bastardkb/charybdis/3x5/v2/splinky_3 -km hawkbane
+qmk flash -kb bastardkb/charybdis/3x5/v2/splinky_3 -km hawkbane
